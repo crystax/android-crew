746fc4637499240e344099a08dbe864b1ed25f5e
Alexander Zhukov <zuav@crystax.net>
2015-02-11 16:55:33 +0300

    Make commit to test 'update' command

--- formula/icu.rb
+++ formula/icu.rb
@@ -2,4 +2,5 @@ class Icu < Formula
   homepage "http://www.icu.org"
 
   release version: '54.1', sha256: 'ed46fab16322b0367849797cf5dd31824f4db29e4ca016b25c8b4e4d6ac2eeee'
+  release version: '54.2', sha256: 'ed46fab16322b0367849797cf5dd31824f4db29e4ca016b25c8b4e4d6ac2eeee'
 end
