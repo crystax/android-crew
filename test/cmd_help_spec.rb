b147aebe8b72e44a95e0c16ff06f8d2d461115e4
Alexander Zhukov <zuav@crystax.net>
2015-04-23 17:21:19 +0100

    Use 'result' helper

--- test/cmd_help_spec.rb
+++ test/cmd_help_spec.rb
@@ -4,7 +4,7 @@ require_relative '../library/cmd/help.rb'
 describe "crew help" do
   it "outputs help info" do
     crew 'help'
+    expect(result).to eq(:ok)
     expect(out).to eq(CREW_HELP)
-    expect(exitstatus).to be_zero
   end
 end
