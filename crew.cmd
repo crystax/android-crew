d72f603417084528561f726223c25855be1f1b4a
Alexander Zhukov <zuav@crystax.net>
2015-04-23 15:22:17 +0100

    Fix tools path

--- crew.cmd
+++ crew.cmd
@@ -8,6 +8,10 @@ set GEM_PATH=
 set CREW_FILE_DIR=%~dp0
 set CREW_RUBY_VERSION=2.2.0
 
+set CREW_HOST_OS=windows
+set CREW_HOST_CPU=x86_64
+rem CREW_32BIT_HOST_CPU=${CREW_HOST_CPU%_64}
+
 if not defined CREW_BASE_DIR (
    set CREW_BASE_DIR=%CREW_FILE_DIR%
 )
@@ -21,14 +25,14 @@ if not defined CREW_NDK_DIR (
 )
 
 if not defined CREW_TOOLS_DIR (
-   set CREW_TOOLS_DIR=%CREW_NDK_DIR%tools\
+   set CREW_TOOLS_DIR=%CREW_NDK_DIR%prebuilt\%CREW_HOST_OS%-%CREW_HOST_CPU%\
 )
 
 if not defined CREW_RUBY_DIR (
-   set CREW_RUBY_DIR=%CREW_TOOLS_DIR%ruby\bin
+   set CREW_RUBY_DIR=%CREW_TOOLS_DIR%bin\
 )
 
-set GIT_EXEC_PATH=%CREW_TOOLS_DIR%git\libexec\git-core
+set GIT_EXEC_PATH=%CREW_TOOLS_DIR%libexec\git-core
 
 rem set CREW
 rem set GIT
