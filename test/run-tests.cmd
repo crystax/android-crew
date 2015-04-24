764b2995ae96336e4da08d4bae4e4d735ac43670
Alexander Zhukov <zuav@crystax.net>
2015-04-24 14:30:51 +0100

    Add script to run crew tests on windows

---
+++ test/run-tests.cmd
@@ -0,0 +1,21 @@
+@echo off
+
+setlocal
+
+set testdir=%~dp0
+set toolsbasedir=%testdir%..\..\..\prebuilt
+if exist %toolsbasedir%\windows-x86_64 (
+   set cpu=-x86_64
+) else (
+  set cpu=
+)
+
+set bindir=%toolsbasedir%\windows%cpu%\bin
+
+echo %bindir%
+
+%bindir%\make
+
+endlocal
+
+exit /b %errorlevel%
