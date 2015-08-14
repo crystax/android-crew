fdde83e0a1e88d26c789e1e3a90caff0f20cde0f
Alexander Zhukov <zuav@crystax.net>
2015-08-14 19:30:23 +0300

    Add ruby formula to utilities dir

--- formula/utilities/curl.rb
+++ formula/utilities/curl.rb
@@ -2,6 +2,4 @@ class Curl < Formula
   homepage "http://curl.haxx.se/"
 
   release version: '7.42.0', build_number: 1, sha256: '0'
-
-  depends_on ''
 end
---
+++ formula/utilities/ruby.rb
@@ -0,0 +1,5 @@
+class Ruby < Formula
+  homepage "https://www.ruby-lang.org/"
+
+  release version: '2.2.2', build_number: 1, sha256: '0'
+end
