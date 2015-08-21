e9a8f6fa45e12d1591011d0a58e34e20073f7f0f
Alexander Zhukov <zuav@crystax.net>
2015-08-21 17:56:32 +0300

    Rename ICU to ICU4C

--- formula/icu4c.rb
+++ formula/icu4c.rb
@@ -1,7 +1,8 @@
-class Icu < Formula
+class Icu4c < Formula
   include Library
 
-  homepage "http://www.icu.org"
+  desc "C/C++ and Java libraries for Unicode and globalization"
+  homepage "http://site.icu-project.org/"
 
   release version: '53.2', crystax_version: 1, sha256: 'ed46fab16322b0367849797cf5dd31824f4db29e4ca016b25c8b4e4d6ac2eeee'
   release version: '54.1', crystax_version: 1, sha256: '1db9d44870aa443a5485038ddbe5dd4340c9565b7562bf92ddfa727f60f1c2bf'
