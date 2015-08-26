a6a61a2bb80babf6b53348c4bc88f43e180f1477
Alexander Zhukov <zuav@crystax.net>
2015-08-26 18:55:21 +0300

    Rename libjpeg to jpeg

---
+++ formula/jpeg.rb
@@ -0,0 +1,8 @@
+class Jpeg < Formula
+  include Library
+
+  desc "JPEG image manipulation library"
+  homepage "http://www.ijg.org"
+
+  release version: '9a', crystax_version: 1, sha256: '2b6925718cf4afb6a16e97e80a37f4b4eaa5e279978de9c1ed28e498ade65dc6'
+end
