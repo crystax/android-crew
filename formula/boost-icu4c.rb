6edd67d618bfc8b1fa7cb7e3ad4cc43880d186f5
Alexander Zhukov <zuav@crystax.net>
2015-08-26 18:55:10 +0300

    Rename ICU to ICU4C

---
+++ formula/boost-icu4c.rb
@@ -0,0 +1,12 @@
+class BoostIcu4c < Formula
+  include Library
+
+  desc "Boost libraries built with ICU4C"
+  homepage "http://www.boost.org"
+
+  release version: '1.57.0', crystax_version: 1, sha256: '0'
+  release version: '1.58.0', crystax_version: 1, sha256: '0'
+  release version: '1.59.0', crystax_version: 1, sha256: '0'
+
+  depends_on 'icu4c'
+end
