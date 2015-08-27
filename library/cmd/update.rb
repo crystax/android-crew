1238152a7e5d184f7f73343755f9ac06476ee699
Alexander Zhukov <zuav@crystax.net>
2015-08-27 23:08:33 +0300

    Modyfi update to show info about utils too

--- library/cmd/update.rb
+++ library/cmd/update.rb
@@ -9,9 +9,10 @@ module Crew
       raise CommandRequresNoArguments
     end
 
-    report = Report.new
     updater = Updater.new
     updater.pull!
+
+    report = Report.new
     report.update(updater.report)
 
     if report.empty?
@@ -48,8 +49,9 @@ module Crew
     end
 
     def report
-      map = Hash.new { |h,k| h[k] = [] }
-      formuladir = Global::FORMULA_DIR.basename.to_s
+      map = { utils: Hash.new { |h,k| h[k] = [] }, libs: Hash.new { |h,k| h[k] = [] } }
+      formula_dir = Global::FORMULA_DIR.basename.to_s
+      utility_dir = File.join(formula_dir, Global::UTULITY_DIR.basename.to_s)
 
       if initial_revision and initial_revision != current_revision
         diff.each_delta do |delta|
@@ -57,15 +59,16 @@ module Crew
           dst = delta.new_file[:path]
 
           next unless File.extname(dst) == ".rb"
-          next unless [src, dst].any? { |p| File.dirname(p) == formuladir }
+          next unless [src, dst].any? { |p| File.dirname(p).start_with?(formula_dir) }
 
           status = delta.status_char.to_s
+          type = (src == utility_dir) ? :utils : :libs
           case status
           when "A", "M", "D"
-            map[status.to_sym] << repository_path.join(src)
+            map[type][status.to_sym] << repository_path.join(src)
           when "R"
-            map[:D] << repository_path.join(src) if File.dirname(src) == formuladir
-            map[:A] << repository_path.join(dst) if File.dirname(dst) == formuladir
+            map[type][:D] << repository_path.join(src) if File.dirname(src) == formula_dir
+            map[type][:A] << repository_path.join(dst) if File.dirname(dst) == formula_dir
           end
         end
       end
@@ -101,40 +104,46 @@ module Crew
 
   class Report
     def initialize
-      @hash = {}
+      @hash = { utils: {}, libs: {} }
     end
 
-    def fetch(*args, &block)
-      @hash.fetch(*args, &block)
-    end
-
-    def update(*args, &block)
-      @hash.update(*args, &block)
+    def update(h)
+      @hash[:utils].update(h[:utils])
+      @hash[:libs].update(h[:libs])
     end
 
     def empty?
-      @hash.empty?
+      @hash[:utils].empty? and @hash[:libs].empty?
     end
 
     def dump
       # Key Legend: Added (A), Copied (C), Deleted (D), Modified (M), Renamed (R)
-      dump_formula_report(:A, "New Formulae")
-      dump_formula_report(:M, "Updated Formulae")
-      dump_formula_report(:D, "Deleted Formulae")
+      dump_formula_report(:utils, :M, "Updated Utilities")
+      dump_formula_report(:utils, :A, "New Utilities")
+      dump_formula_report(:utils, :D, "Deleted Utilities")
+      dump_formula_report(:libs, :A, "New Formulae")
+      dump_formula_report(:libs, :M, "Updated Formulae")
+      dump_formula_report(:libs, :D, "Deleted Formulae")
     end
 
-    def select_formula(key)
-      fetch(key, []).map do |path|
-          path.basename(".rb").to_s
-      end.sort
-    end
+    private
 
-    def dump_formula_report(key, title)
-      formula = select_formula(key)
+    def dump_formula_report(type, key, title)
+      formula = select_formula(type, key)
       unless formula.empty?
         puts "==> #{title}"
         puts formula
       end
+    end
+
+    def select_formula(type, key)
+      fetch(type, key, []).map do |path|
+          path.basename(".rb").to_s
+      end.sort
+    end
+
+    def fetch(type, *args)
+      @hash[type].fetch(*args)
     end
   end
 end
