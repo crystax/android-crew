72cededf3cf6d5c12398baba732029644b310303
Alexander Zhukov <zuav@crystax.net>
2015-09-17 12:35:00 +0300

    Fix typos and errors
    test_prepare.rb works as expected now.

--- test/test_prepare.rb
+++ test/test_prepare.rb
@@ -3,26 +3,27 @@ require 'digest'
 require 'pathname'
 require 'json'
 require_relative '../library/release.rb'
+require_relative '../library/utils.rb'
 require_relative 'test_consts.rb'
 
 
-TOOLS_DIR          = ENV['CREW_RUBY_DIR']
-PLATFORM           = File.basename(TOOLS_DIR)
-UTILS_DOWNLOAD_DIR = File.join(Crew_test::DOCROOT_DIR, 'utilities')
-ORIG_NDK_DIR       = File.join('..', '..', '..')
-ORIG_TOOLS_DIR     = File.join(ORIG_NDK_DIR, 'prebuilt', PLATFORM)
+tools_dir          = ENV['CREW_TOOLS_DIR']
+PLATFORM           = File.basename(tools_dir)
+utils_download_dir = File.join(Crew_test::DOCROOT_DIR, 'utilities')
+orig_ndk_dir       = File.join('..', '..', '..')
+orig_tools_dir     = File.join(orig_ndk_dir, 'prebuilt', PLATFORM)
 
 # copy utils from NDK dir to tests directory structure
-FileUtils.mkdir_p File.dirname(TOOLS_DIR)
-FileUtils.mkdir_p UTILS_DOWNLOAD_DIR
-FileUtils.cp_r File.join(ORIG_TOOLS_DIR, 'crew') TOOLS_DIR
-FileUtils.cp_r File.join(ORIG_TOOLS_DIR, 'bin')  TOOLS_DIR
+FileUtils.mkdir_p File.dirname(File.join(tools_dir, 'crew'))
+FileUtils.mkdir_p utils_download_dir
+FileUtils.cp_r File.join(orig_tools_dir, 'crew'), File.join(tools_dir, 'crew')
+FileUtils.cp_r File.join(orig_tools_dir, 'bin'),  tools_dir
 
-ORIG_NDK_DIR       = Pathname.new(ORIG_NDK_DIR).realpath.to_s
-ORIG_TOOLS_DIR     = Pathname.new(ORIG_TOOLS_DIR).realpath.to_s
-ORIG_FORMULA_DIR   = Pathname.new(File.join(NDK_DIR, 'tools', 'crew', 'formula', 'utilities')).realpath.to_s
-TOOLS_DIR          = Pathname.new(TOOLS_DIR).realpath.to_s
-UTILS_DOWNLOAD_DIR = Pathname.new(UTILS_DOWNLOAD_DIR).realpath.to_s
+ORIG_NDK_DIR       = Pathname.new(orig_ndk_dir).realpath.to_s
+ORIG_TOOLS_DIR     = Pathname.new(orig_tools_dir).realpath.to_s
+ORIG_FORMULA_DIR   = Pathname.new(File.join(orig_ndk_dir, 'tools', 'crew', 'formula', 'utilities')).realpath.to_s
+TOOLS_DIR          = Pathname.new(tools_dir).realpath.to_s
+UTILS_DOWNLOAD_DIR = Pathname.new(utils_download_dir).realpath.to_s
 DATA_DIR           = Pathname.new(Crew_test::DATA_DIR).realpath.to_s
 NDK_DIR            = Pathname.new(Crew_test::NDK_DIR).realpath.to_s
 
@@ -65,7 +66,8 @@ def create_archive(orig_release, release, util)
   archive_path = File.join(UTILS_DOWNLOAD_DIR, util, "#{util}-#{release}-#{PLATFORM}.7z")
   FileUtils.mkdir_p File.dirname(archive_path)
   FileUtils.cd('tmp') do
-    cmd = "#{File.join(ORIG_TOOLS_DIR, 'bin', 'p7zip')} a #{archive_path} #{dir_to_archive}"
+    args = ['a', archive_path, dir_to_archive]
+    Utils.run_command(File.join(ORIG_TOOLS_DIR, 'bin', '7za'), *args)
   end
   # rename new release back to old
   FileUtils.cd(util_dir) { FileUtils.mv new, old if old != new }
@@ -92,7 +94,7 @@ curl_releases = [Release.new('7.42.0', 1), Release.new('7.42.0', 3), Release.new
   r.shasum = create_archive(orig_releases['curl'], r, 'curl')
   r
 end
-curl_formula = File.join(FORMULA_DIR, 'curl.rb')
+curl_formula = File.join(ORIG_FORMULA_DIR, 'curl.rb')
 File.open(File.join(DATA_DIR, 'curl-1.rb'), 'w') { |f| f.puts replace_releases(curl_formula, curl_releases.slice(0, 1)) }
 File.open(File.join(DATA_DIR, 'curl-2.rb'), 'w') { |f| f.puts replace_releases(curl_formula, curl_releases.slice(0, 2)) }
 File.open(File.join(DATA_DIR, 'curl-3.rb'), 'w') { |f| f.puts replace_releases(curl_formula, curl_releases) }
@@ -100,11 +102,11 @@ File.open(File.join(DATA_DIR, 'curl-3.rb'), 'w') { |f| f.puts replace_releases(c
 # create archives and formulas for p7zip
 p7zip_release = Release.new('9.20.1', 1)
 p7zip_release.shasum = create_archive(orig_releases['p7zip'], p7zip_release, 'p7zip')
-p7zip_formula = File.join(FORMULA_DIR, 'p7zip.rb')
+p7zip_formula = File.join(ORIG_FORMULA_DIR, 'p7zip.rb')
 File.open(File.join(DATA_DIR, 'p7zip-1.rb'), 'w') { |f| f.puts replace_releases(p7zip_formula, [p7zip_release]) }
 
 # create archives and formulas for ruby
 ruby_release = Release.new('2.2.2', 1)
 ruby_release.shasum = create_archive(orig_releases['ruby'], ruby_release, 'ruby')
-ruby_formula = File.join(FORMULA_DIR, 'ruby.rb')
+ruby_formula = File.join(ORIG_FORMULA_DIR, 'ruby.rb')
 File.open(File.join(DATA_DIR, 'ruby-1.rb'), 'w') { |f| f.puts replace_releases(ruby_formula, [ruby_release]) }
